a1f98398e57bf9fd2d704d8ad1bf64f97fbff8ee
Ramko9999 <ramapitchala@gmail.com>
2021-11-01 02:17:01 -0500

    Removed config

--- client/src/config.ts
+++
@@ -1,9 +0,0 @@
-const productionUrl = "https://ramapitchala.pythonanywhere.com";
-const developmentUrl = "http://127.0.0.1:5000"
-
-const CONFIG = {
-    URL: developmentUrl
-}
-
-
-export default CONFIG;
