3b5231ab34aa5c3cda322a0b04d6c379c27a88a4
Ramko9999 <ramapitchala@gmail.com>
2022-09-04 16:16:20 -0500

    updated experience and education

--- server/src/education.py
+++ server/src/education.py
@@ -1,15 +1,18 @@
 
 education = [
     {
-        "name": "University of Minnesota",
+        "name": "University of Minnesota Twin Cities",
         "logo": "https://media.glassdoor.com/sqll/498739/university-of-minnesota-duluth-squarelogo-1424347899660.png",
         "position": "Computer Science Student",
         "from": "Sept. 2019",
-        "to": "Dec. 2022",
-        "background": '''Pursuing a Bachelor's in Computer Science. Finished core courses such as Algorithms & Data Structures, 
-                      Machine Organization, Advanced Programming Principles, and Discrete Structures. There is a really nice gym on campus.''',
-        "location": "Minneapolis, Minnesota",
-        "points": []
+        "to": "May. 2023",
+        "background": "",
+        "points": [
+            "Pursuing a Bachelor's of Science in Computer Science",
+            "Notable Coursework: Algorithms & Data Structures, OS, Introduction to ML, and Software Design and Development",
+            "Attempting to utilize the nice gym on campus"
+        ],
+        "location": "Minneapolis, Minnesota"
     },
     {
         "name": "Normandale College",
@@ -17,17 +20,9 @@ education = [
         "position": "PSEO Student",
         "from": "Aug. 2018",
         "to": "May 2020",
-        "background": "Took PSEO courses in Physics, Computer Science, History and Literature. The basketballs in the gym were always deflated.",
+        "background": "",
         "location": "Bloomington, Minnesota",
-        "points": []
-    },
-    {
-        "name": "Eden Prairie High School",
-        "logo": "https://ih1.redbubble.net/image.1486294349.0030/st,small,507x507-pad,600x600,f8f8f8.jpg",
-        "position": "Student",
-        "from": "Sept. 2016",
-        "to": "May 2020",
-        "background": "Memed around with friends in AP Computer Science, played some video games, and really took advantage of PSEO.",
-        "location": "Eden Prairie, Minnesota",
-        "points": []
+        "points": [
+            "Enrolled in various coursework for dual-enrollment"
+        ]
     }]
